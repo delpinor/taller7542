b365e3c130416d2bed7a6681e2f8fd507637729a
Matias Gutierrez <matiasgutierrez@outlook.com>
2019-05-15 19:13:45 -0300

    mas cambios...

--- Marvel vs Capcom/View/View_Jugador_CapAmericaBlack.h
+++ Marvel vs Capcom/View/View_Jugador_CapAmericaBlack.h
@@ -1,7 +1,6 @@
 #ifndef VIEW_JUGADOR_CAPAMERICABLACK_H_
 #define VIEW_JUGADOR_CAPAMERICABLACK_H_
 #include <SDL2/SDL.h>
-#include "../View/View_Jugador.h"
 #include "../View/View_Jugador_CapAmerica.h"
 
 class View_Jugador_CapAmericaBlack: public View_Jugador_CapAmerica{
